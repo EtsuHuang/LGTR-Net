4e296601f6dabe33ac268926fc09e6d72dbd5377
Y.Huang <50124026+EtsuHuang@users.noreply.github.com>
2024-12-19 09:55:33 +0800

    Update utils_fit.py

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -28,36 +28,17 @@ def fit_one_epoch(model_train, model, loss_history, optimizer, epoch, epoch_step
             if cuda:
                 images  = images.cuda(local_rank)
                 targets = targets.cuda(local_rank)
-                
-        #----------------------#
-        #   清零梯度
-        #----------------------#
         optimizer.zero_grad()
         if not fp16:
-            #----------------------#
-            #   前向传播
-            #----------------------#
             outputs     = model_train(images)
-            #----------------------#
-            #   计算损失
-            #----------------------#
             loss_value  = nn.CrossEntropyLoss()(outputs, targets)
             loss_value.backward()
             optimizer.step()
         else:
             from torch.cuda.amp import autocast
             with autocast():
-                #----------------------#
-                #   前向传播
-                #----------------------#
                 outputs     = model_train(images)
-                #----------------------#
-                #   计算损失
-                #----------------------#
                 loss_value  = nn.CrossEntropyLoss()(outputs, targets)
-            #----------------------#
-            #   反向传播
-            #----------------------#
             scaler.scale(loss_value).backward()
             scaler.step(optimizer)
             scaler.update()
@@ -109,10 +90,6 @@ def fit_one_epoch(model_train, model, loss_history, optimizer, epoch, epoch_step
         loss_history.append_loss(epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)
         print('Epoch:' + str(epoch + 1) + '/' + str(Epoch))
         print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
-        
-        #-----------------------------------------------#
-        #   保存权值
-        #-----------------------------------------------#
         if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
             torch.save(model.state_dict(), os.path.join(save_dir, "ep%03d-loss%.3f-val_loss%.3f.pth" % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
 
