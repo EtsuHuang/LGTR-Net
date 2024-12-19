d076ba73b7ebeaa5688598a1d5acb13807abf8ff
Y.Huang <50124026+EtsuHuang@users.noreply.github.com>
2024-12-19 09:51:09 +0800

    Update classification.py

--- classification.py
+++ classification.py
@@ -7,43 +7,14 @@ from nets import get_model_from_name
 from utils.utils import (cvtColor, get_classes, letterbox_image,
                          preprocess_input, show_config)
 
-
-#--------------------------------------------#
-#   使用自己训练好的模型预测需要修改3个参数
-#   model_path和classes_path和backbone都需要修改！
-#--------------------------------------------#
 class Classification(object):
     _defaults = {
-        #--------------------------------------------------------------------------#
-        #   使用自己训练好的模型进行预测一定要修改model_path和classes_path！
-        #   model_path指向logs文件夹下的权值文件，classes_path指向model_data下的txt
-        #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
-        #--------------------------------------------------------------------------#
         "model_path"        : 'model_data/params/part (12).pth',
         "classes_path"      : 'model_data/sssc_10.txt',
         # "classes_path": 'model_data/cls_classes.txt',
-        #--------------------------------------------------------------------#
-        #   输入的图片大小
-        #--------------------------------------------------------------------#
         "input_shape"       : [224, 224],
-        #--------------------------------------------------------------------#
-        #   所用模型种类：
-        #   mobilenetv2、
-        #   resnet18、resnet34、resnet50、resnet101、resnet152
-        #   vgg11、vgg13、vgg16、vgg11_bn、vgg13_bn、vgg16_bn、
-        #   vit_b_16、
-        #   swin_transformer_tiny、swin_transformer_small、swin_transformer_base
-        #--------------------------------------------------------------------#
         "backbone"          : 'Lgtr_Netv1',
-        #--------------------------------------------------------------------#
-        #   该变量用于控制是否使用letterbox_image对输入图像进行不失真的resize
-        #   否则对图像进行CenterCrop
-        #--------------------------------------------------------------------#
         "letterbox_image"   : False,
-        #-------------------------------#
-        #   是否使用Cuda
-        #   没有GPU可以设置成False
-        #-------------------------------#
         "cuda"              : True
     }
 
@@ -53,30 +24,15 @@ class Classification(object):
             return cls._defaults[n]
         else:
             return "Unrecognized attribute name '" + n + "'"
-
-    #---------------------------------------------------#
-    #   初始化classification
-    #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)
-
-        #---------------------------------------------------#
-        #   获得种类
-        #---------------------------------------------------#
         self.class_names, self.num_classes = get_classes(self.classes_path)
         self.generate()
         
         show_config(**self._defaults)
-
-    #---------------------------------------------------#
-    #   获得所有的分类
-    #---------------------------------------------------#
     def generate(self):
-        #---------------------------------------------------#
-        #   载入模型与权值
-        #---------------------------------------------------#
         if self.backbone not in ['vit_b_16', 'swin_transformer_tiny', 'swin_transformer_small', 'swin_transformer_base']:
             self.model  = get_model_from_name[self.backbone](num_classes = self.num_classes, pretrained = False)
         else:
@@ -89,42 +45,18 @@ class Classification(object):
         if self.cuda:
             self.model = nn.DataParallel(self.model)
             self.model = self.model.cuda()
-
-    #---------------------------------------------------#
-    #   检测图片
-    #---------------------------------------------------#
     def detect_image(self, image):
-        #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-        #---------------------------------------------------------#
         image       = cvtColor(image)
-        #---------------------------------------------------#
-        #   对图片进行不失真的resize
-        #---------------------------------------------------#
         image_data  = letterbox_image(image, [self.input_shape[1], self.input_shape[0]], self.letterbox_image)
-        #---------------------------------------------------------#
-        #   归一化+添加上batch_size维度+转置
-        #---------------------------------------------------------#
         image_data  = np.transpose(np.expand_dims(preprocess_input(np.array(image_data, np.float32)), 0), (0, 3, 1, 2))
 
         with torch.no_grad():
             photo   = torch.from_numpy(image_data)
             if self.cuda:
                 photo = photo.cuda()
-            #---------------------------------------------------#
-            #   图片传入网络进行预测
-            #---------------------------------------------------#
             preds   = torch.softmax(self.model(photo)[0], dim=-1).cpu().numpy()
-        #---------------------------------------------------#
-        #   获得所属种类
-        #---------------------------------------------------#
         class_name  = self.class_names[np.argmax(preds)]
         probability = np.max(preds)
-
-        #---------------------------------------------------#
-        #   绘图并写字
-        #---------------------------------------------------#
         plt.subplot(1, 1, 1)
         plt.imshow(np.array(image))
         plt.title('Class:%s Probability:%.3f' %(class_name, probability))
@@ -133,36 +65,19 @@ class Classification(object):
 
     def get_FPS(self, image, test_interval):
         image_shape = np.array(np.shape(image)[0:2])
-        # ---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-        # ---------------------------------------------------------#
         image = cvtColor(image)
-        # ---------------------------------------------------------#
-        #   给图像增加灰条，实现不失真的resize
-        #   也可以直接resize进行识别
-        # ---------------------------------------------------------#
         image_data = letterbox_image(image, [self.input_shape[1], self.input_shape[0]], self.letterbox_image)
-        # ---------------------------------------------------------#
-        #   添加上batch_size维度
-        # ---------------------------------------------------------#
         image_data = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
 
         with torch.no_grad():
             photo = torch.from_numpy(image_data)
             if self.cuda:
                 photo = photo.cuda()
-            # ---------------------------------------------------#
-            #   图片传入网络进行预测
-            # ---------------------------------------------------#
             preds = torch.softmax(self.model(photo)[0], dim=-1).cpu().numpy()
 
         t1 = time.time()
         for _ in range(test_interval):
             with torch.no_grad():
-                # ---------------------------------------------------------#
-                #   将图像输入网络当中进行预测！
-                # ---------------------------------------------------------#
                 preds = torch.softmax(self.model(photo)[0], dim=-1).cpu().numpy()
 
         t2 = time.time()
