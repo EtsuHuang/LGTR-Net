009d92f13aad6dfc0b707652e97c1ae3b86817a6
Y.Huang <50124026+EtsuHuang@users.noreply.github.com>
2024-12-19 09:47:34 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -13,27 +13,14 @@ metrics_out_path        = "metrics_out"
 
 class Eval_Classification(Classification):
     def detect_image(self, image):        
-        #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-        #---------------------------------------------------------#
         image       = cvtColor(image)
-        #---------------------------------------------------#
-        #   对图片进行不失真的resize
-        #---------------------------------------------------#
         image_data  = letterbox_image(image, [self.input_shape[1], self.input_shape[0]], self.letterbox_image)
-        #---------------------------------------------------------#
-        #   归一化+添加上batch_size维度+转置
-        #---------------------------------------------------------#
         image_data  = np.transpose(np.expand_dims(preprocess_input(np.array(image_data, np.float32)), 0), (0, 3, 1, 2))
 
         with torch.no_grad():
             photo   = torch.from_numpy(image_data).type(torch.FloatTensor)
             if self.cuda:
                 photo = photo.cuda()
-            #---------------------------------------------------#
-            #   图片传入网络进行预测
-            #---------------------------------------------------#
             preds   = torch.softmax(self.model(photo)[0], dim=-1).cpu().numpy()
 
         return preds
